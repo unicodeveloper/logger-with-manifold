0c32f358006f9e24925ce9a9a0ad78c8adfd518e
unicodeveloper <prosperotemuyiwa@gmail.com>
2017-06-11 17:53:42 -0400

    Change url

--- src/utils/logger-api.js
+++ src/utils/logger-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5555';
+const BASE_URL = 'https://loggerapp-jxruffzvrv.now.sh';
 
 export function logNewApp(data) {
   const url = `${BASE_URL}/api/log`;
